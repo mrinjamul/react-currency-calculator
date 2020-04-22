b98806b4ef11aaa7c1de4fc1b21814a4b2c2546f
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2020-04-22 17:38:54 +0530

    Updates

---
+++ precache-manifest.d95dc19d47478eee70f1f547323375fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9166aa0ad6654bd6ddf73e19bd9b70d",
+    "url": "/react-currency-calculator/index.html"
+  },
+  {
+    "revision": "6cdd34c387fa3538684d",
+    "url": "/react-currency-calculator/static/css/main.2739fe3a.chunk.css"
+  },
+  {
+    "revision": "2bb1f8c7e263117a9e25",
+    "url": "/react-currency-calculator/static/js/2.99105cd7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-currency-calculator/static/js/2.99105cd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cdd34c387fa3538684d",
+    "url": "/react-currency-calculator/static/js/main.4bc8075e.chunk.js"
+  },
+  {
+    "revision": "97b24dd5cc30d03b2624",
+    "url": "/react-currency-calculator/static/js/runtime-main.78bc2951.js"
+  }
+]);
